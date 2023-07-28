6c748670373733cfb462ab030bcb79a1df771f93
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-28 16:45:13 +0530

    .

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
         <h1>{{$post->title}}</h1>
 
         <p>
-            {!!$post->body!!} {{-- $post->body contains <p> tag which will be escaped if double curly braces is used --}}
+            {{$post->body}}
         </p>
     </article>
 
