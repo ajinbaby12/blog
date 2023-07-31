ee273780f22d31b9674cc210c4887ee6a4fe7893
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-31 12:26:37 +0530

    .

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,11 +1,14 @@
 <x-layout>
     @foreach ($posts as $post)
     <article>
-        <h1><a href="/post/{{$post->slug}}">
+        <h1>
+            <a href="/post/{{$post->slug}}">
                 {{$post->title}}
-            </a></h1>
+            </a>
+        </h1>
         <p>
-            <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+            <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}
+            </a>
         </p>
         {{$post->excerpt}}
     </article>
