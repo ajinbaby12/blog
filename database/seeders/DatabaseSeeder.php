993406a47770f2195a49af147993abe8da71c2b7
ajinbaby01 <ajin.baby@bankopen.co>
2023-07-31 16:59:38 +0530

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -67,23 +67,5 @@ class DatabaseSeeder extends Seeder
         //     'excerpt' => 'Second post excerpt',
         //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         // ]);
-
-        // Post::create([
-        //     'user_id' => $user->id,
-        //     'category_id' => $hobby->id,
-        //     'slug' => 'my-hobby-post-1',
-        //     'title' => 'My First hobby Post',
-        //     'excerpt' => 'Third post excerpt',
-        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        // ]);
-
-        // Post::create([
-        //     'user_id' => $user->id,
-        //     'category_id' => $work->id,
-        //     'slug' => 'my-work-post-2',
-        //     'title' => 'My Second work Post',
-        //     'excerpt' => 'Fourth post excerpt',
-        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        // ]);
     }
 }
